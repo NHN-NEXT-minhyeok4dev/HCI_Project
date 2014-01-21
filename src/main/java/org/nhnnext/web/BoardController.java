6450c7368eff87bae0b4271329d4f225a5548e87
jkc1352000 <jkc1352000@nhnnext.org>
2014-01-21 13:42:03 +0900

    db update

--- src/main/java/org/nhnnext/web/BoardController.java
+++ src/main/java/org/nhnnext/web/BoardController.java
@@ -1,6 +1,7 @@
 package org.nhnnext.web;
 
 import org.nhnnext.repository.BoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class BoardController {
 
+	@Autowired
     private BoardRepository boardRepository;
 	
 	@RequestMapping(value="/board", method=RequestMethod.POST)
