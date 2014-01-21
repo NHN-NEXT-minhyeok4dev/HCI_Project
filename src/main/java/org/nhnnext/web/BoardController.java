ecbe71780037b3f8a328fd65c681ec7e63c05093
astomusic <astomusic@naver.com>
2014-01-21 16:14:57 +0900

    이미지파일 업로드 연동 완료 추후작업 PDF파일로 전환 저장 폴더 절대 경로 설정 관련

--- src/main/java/org/nhnnext/web/BoardController.java
+++ src/main/java/org/nhnnext/web/BoardController.java
@@ -1,10 +1,12 @@
 package org.nhnnext.web;
 
 import org.nhnnext.repository.BoardRepository;
+import org.nhnnext.support.FileUploader;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class BoardController {
@@ -13,9 +15,10 @@ public class BoardController {
     private BoardRepository boardRepository;
 	
 	@RequestMapping(value="/board", method=RequestMethod.POST)
-	public String create(Board board) {
-//		System.out.println("title : " + title + " contents : " + contents);
+	public String create(Board board, MultipartFile file) {
+		FileUploader.upload(file);
 		System.out.println("board:"+ board);
+		 board.setFileName(file.getOriginalFilename());
 		boardRepository.save(board);
 		return "redirect:/";
 	}
