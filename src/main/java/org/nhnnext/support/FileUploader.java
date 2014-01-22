cd2db0591521707c76f76dc860a5e219d0f1500c
astomusic <astomusic@naver.com>
2014-01-22 13:09:53 +0900

    update file upload folder

--- src/main/java/org/nhnnext/support/FileUploader.java
+++ src/main/java/org/nhnnext/support/FileUploader.java
@@ -5,7 +5,10 @@ import java.io.File;
 import org.springframework.web.multipart.MultipartFile;
 
 public class FileUploader {
-	private static final String ATTACHMENT_ROOT_DIR = "/Users/asto/Documents/workspace/HCI_Project/webapp/pdf";
+	//Local Path
+	//private static final String ATTACHMENT_ROOT_DIR = "/Users/asto/Documents/workspace/HCI_Project/webapp/pdf";
+	//Server Path
+	private static final String ATTACHMENT_ROOT_DIR = "/var/lib/tomcat7/webapps/ROOT/pdf";
 
 	public static String upload(MultipartFile multipartFile) {
 		if (multipartFile.isEmpty()) {
